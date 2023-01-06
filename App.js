c886c81d5670b1dcca3737f0d6397b2af95e3208
Pragati Gupta <connect2pragatiguptaa@gmail.com>
2023-01-07 00:27:12 +0530

    A3Q1-Fina  Created Nested header using jsx element

--- App.js
+++ App.js
@@ -2,10 +2,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+const nestedHeader =    (<div className ="title">
+                            <h1>Heading 1 by JSX</h1>
+                            <h2>Heading 2 by JSX</h2>
+                            <h3>Heading 3 by JSX</h3>
+                        </div>);
 
-const heading1 = <h1>Heading 1 by JSX</h1>
-const heading2 = <h2>Heading 2 by JSX</h2>
-const heading3 = <h3>Heading 3 by JSX</h3>
-const divContainer = React.createElement("div", {className:"title"}, [heading1, heading2, heading3]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(divContainer);
+root.render(nestedHeader);
