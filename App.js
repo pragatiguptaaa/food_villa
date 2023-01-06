f9a16487c74d6354681530df73ceb5a7219b12d4
Pragati Gupta <connect2pragatiguptaa@gmail.com>
2023-01-07 00:29:21 +0530

    A3Q1  Created Nested header using jsx functional componenet

--- App.js
+++ App.js
@@ -2,11 +2,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const nestedHeader =    (<div className ="title">
-                            <h1>Heading 1 by JSX</h1>
-                            <h2>Heading 2 by JSX</h2>
-                            <h3>Heading 3 by JSX</h3>
-                        </div>);
+const NestedHeader =  () =>  {
+return (<div className ="title">
+             <h1>Heading 1 by JSX</h1>
+             <h2>Heading 2 by JSX</h2>
+             <h3>Heading 3 by JSX</h3>
+         </div>);
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(nestedHeader);
+root.render(<NestedHeader />);
