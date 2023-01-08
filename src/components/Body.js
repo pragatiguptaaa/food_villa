c709975acb134d6af71307311ee907702ecae4a1
Pragati Gupta <connect2pragatiguptaa@gmail.com>
2023-01-08 15:26:24 +0530

    Added search input and button Used normal local variable to change the text on search input and button- Doesn't work

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,14 +2,30 @@ import RestaurantCard from './RestaurantCard.js';
 import { restaurantList } from '../constants.js';
 
 const Body = () =>{
+    const searchText ="KFC";
     return (
-    <div className="restaurant-list">
-        {
-            restaurantList.map((restaurant) =>{ 
-                return (<RestaurantCard {...restaurant.data} key ={restaurant.data.id}/> )
-            })
-        }
-        
-    </div>);
+        <>
+            <div className = "search-container">
+                <input 
+                    className="search-input" 
+                    type="text" 
+                    placeholder="search"
+                    value={searchText}
+                    onChange = {(e) =>{
+                        searchText = e.target.value;
+                    }} >
+                </input>
+                <button className='="search-button'> Search {searchText}</button>
+            </div>
+            <div className="restaurant-list">
+                {
+                    restaurantList.map((restaurant) =>{ 
+                        return (<RestaurantCard {...restaurant.data} key ={restaurant.data.id}/> )
+                    })
+                }
+                
+            </div>
+        </>
+    );
 };
 export default Body;
