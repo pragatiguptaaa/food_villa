72a13f9f93244f715e13029a9e85141f91448a19
Pragati Gupta <connect2pragatiguptaa@gmail.com>
2023-01-23 17:17:58 +0530

    passed props from parent(AboutUs.js) and accessed in ProfileClass.js

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,13 +1,14 @@
 import React from "react";
 import {Outlet} from "react-router-dom";
 import Profile from "./Profile";
+import ProfileClass from "./ProfileClass";
 
 const AboutUs= () => {
     return(
         <>
             <h1>Welcome to the About page!!!!</h1>
             <Profile/>
-            <Outlet />
+            <ProfileClass name="Pragati" />
         </>
     );
 };
--- src/components/ProfileClass.js
+++ src/components/ProfileClass.js
@@ -3,8 +3,14 @@ import { render } from "react-dom";
 
 class ProfileClass extends React.Component{
     render(){
-        return <h1> Profile class compoent</h1>;
+        return (
+            <>
+             <h1> Profile class compoent</h1>
+             <h2> Your name: {this.props.name} </h2>
+            </>
+        );
     }
 }
+
 
 export default ProfileClass;
