e87d38289b165562c8a759988c7be1ab4a1bd1c5
Pragati Gupta <connect2pragatiguptaa@gmail.com>
2023-01-23 17:24:47 +0530

    Used consturctor to create state variable count

--- src/components/ProfileClass.js
+++ src/components/ProfileClass.js
@@ -2,11 +2,23 @@ import React from "react";
 import { render } from "react-dom";
 
 class ProfileClass extends React.Component{
+
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            count:0
+        }
+    }
+    
     render(){
+        const {count} = this.state;
         return (
             <>
              <h1> Profile class compoent</h1>
-             <h2> Your name: {this.props.name} </h2>
+             <h2> Your name: {this.props.name}, Count: {count} </h2>
+             <button onClick={() => this.setState({count: count+1})}> Click me to increase count</button>
+
             </>
         );
     }
