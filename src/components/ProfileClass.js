6995a315dce826ff6f0df43918f6d29a36cb26bb
Pragati Gupta <connect2pragatiguptaa@gmail.com>
2023-01-23 19:08:11 +0530

    Added componentWillUnmont function TODO: Make it SPA

--- src/components/ProfileClass.js
+++ src/components/ProfileClass.js
@@ -13,17 +13,26 @@ class ProfileClass extends React.Component{
         }
     }
 
-    async componentDidMount(){
+     componentDidMount(){
         console.log("Profile - Child - ComponentDidMount - Started");
-        const data = await fetch("https://www.google-analytics.com/j/collect?v=1&_v=j99&a=1812485248&t=pageview&_s=1&dl=https%3A%2F%2Fwww.swiggy.com%2Frestaurants%2Fbhartiya-jalpan-varthur-sarjapur-road-bangalore-628617&ul=en-us&de=UTF-8&dt=Bhartiya%20Jalpan%20%7C%20Home%20delivery%20%7C%20Order%20online%20%7C%20Varthur%20Sarjapur%20Road%20Bangalore&sd=24-bit&sr=1536x864&vp=222x697&je=0&_u=QACAAEABAAAAACAAI~&jid=&gjid=&cid=950034582.1673104011&tid=0&_gid=667178416.1674479112&_slc=1&gtm=2wg1i0MJV7Q8Q&z=1699936068");
-        // const json = await data.json();
-        this.setState({userInfo: {name:"hello"}});
+
+        this.timer = setInterval(()=>console.log("I am logging after every sec"), 1000);
+
+        //API Call
+        // const data = await fetch("https://www.google-analytics.com/j/collect?v=1&_v=j99&a=1812485248&t=pageview&_s=1&dl=https%3A%2F%2Fwww.swiggy.com%2Frestaurants%2Fbhartiya-jalpan-varthur-sarjapur-road-bangalore-628617&ul=en-us&de=UTF-8&dt=Bhartiya%20Jalpan%20%7C%20Home%20delivery%20%7C%20Order%20online%20%7C%20Varthur%20Sarjapur%20Road%20Bangalore&sd=24-bit&sr=1536x864&vp=222x697&je=0&_u=QACAAEABAAAAACAAI~&jid=&gjid=&cid=950034582.1673104011&tid=0&_gid=667178416.1674479112&_slc=1&gtm=2wg1i0MJV7Q8Q&z=1699936068");
+        // // const json = await data.json();
+        // this.setState({userInfo: {name:"hello"}});
         console.log("Profile - Child - ComponentDidMount - Finished");
     }
 
     componentDidUpdate()
     {
         console.log("Profile - Child - ComponentDidUpdate");
+    }
+
+    componentWillUnmout(){
+        clearInterval(this.timer);
+        console.log("Profile - Child - Compoennt will Unmount");
     }
 
     render(){
