e13ed5af7bde397ce502eca81bbd5cba2ae287ac
Pragati Gupta <connect2pragatiguptaa@gmail.com>
2023-03-20 15:26:48 +0530

    Added functionality to show only one accordian at atime and collapse all other accordians(TODO: Click on current accordian to collapse it)

--- src/components/Instamart.js
+++ src/components/Instamart.js
@@ -1,16 +1,14 @@
 import React from "react";
 import {useState} from "react";
 
-const Section = ({title, description}) =>{
-
-const [visible, setVisible] = useState(false);
+const Section = ({title, description, visible, setVisible}) =>{
 
 return (
     <>
     <div className="border border-black p-2 m-2">
         <h1 className="text-xl font-bold ">{title}</h1>
         <button 
-            onClick = {()=>setVisible(!visible)}>
+            onClick = {()=>setVisible()}>
             { visible ? "Hide Description": "Show Description"}
         </button>
         {visible && <p>{description}</p>}
@@ -19,12 +17,23 @@ return (
 )}
 
 const Instamart = () =>{
+    const [visibleState, setVisibleState] = useState({
+        about: false,
+        careers: false,
+        contacts: false
+    });
     return(
         <>
            <h1 className="text-3xl p-2 m-2 font-bold"> Welcome to the Instamart page!!!</h1>
-           <Section title ="About Instamart" description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."></Section>
-           <Section title ="Careers" description = "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section 1.10.32."></Section>
-           <Section title ="Contacts" description = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."></Section>
+           <Section title ="About Instamart" description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." 
+           visible = {visibleState.about} 
+           setVisible = {() => setVisibleState({about: true, careers: false, contacts: false}) }></Section>
+           <Section title ="Careers" description = "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section 1.10.32." 
+           visible = {visibleState.careers} 
+           setVisible = {() => setVisibleState({about: false, careers: true, contacts: false}) }></Section>
+           <Section title ="Contacts" description = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)." 
+           visible = {visibleState.contacts} 
+           setVisible = {() => setVisibleState({about: false, careers: false, contacts: true}) }></Section>
         </>
     )
 }
