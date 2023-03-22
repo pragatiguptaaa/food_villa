a47e13857624b469bb73b422403e3fd6ac1810a3
Pragati Gupta <connect2pragatiguptaa@gmail.com>
2023-03-22 16:16:06 +0530

    Successfully ran 1st test by mocking images, using Redux Provider and static router

--- src/test/testCases/Header.test.js
+++ src/test/testCases/Header.test.js
@@ -1,7 +1,15 @@
 import {render} from '@testing-library/react';
+import { Provider } from 'react-redux';
+import {StaticRouter} from "react-router-dom/server"
+
+import appReduxStore from '../../utils/AppReduxStore';
 import Header from "../../components/Header";
 
 test("Should load the Food Villa Logo", () =>{
-    const header = render(<Header/>);
+    const header = render(<StaticRouter>
+                                    <Provider store ={appReduxStore}>
+                                                        <Header/>
+                                                    </Provider>
+                          </StaticRouter>);
     console.log(header);
 })
